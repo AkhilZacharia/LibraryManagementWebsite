cd902957b5ee5eb75a940533ffbb260d11cfa991
AkhilZacharia <akhilzacharia35@gmail.com>
2020-06-26 12:18:53 +0530

    db integration update delete

--- src/routes/booksRouter.js
+++ src/routes/booksRouter.js
@@ -1,35 +1,39 @@
 const express = require('express');
-
+const Bookdata = require('../model/bookdata');
 const booksRouter = express.Router();
 function router(nav){
-var books = [
-    {
-        title:'tom n jerry',
-        author:"J Barberra",
-        genre:'Cartoon',
-        img:'tom.jpg'
-    },
-    {
-        title:'Harry Potter',
-        author:"J K Rowling",
-        genre:'Fantasy',
-        img:'harry.jpg'
-    },
-    {
-        title:'Paathummayude Aadu',
-        author:"Basheer",
-        genre:'Novel',
-        img:'basheer.jpg'
-    }
-];
+// var books = [
+//     {
+//         title:'tom n jerry',
+//         author:"J Barberra",
+//         genre:'Cartoon',
+//         img:'tom.jpg'
+//     },
+//     {
+//         title:'Harry Potter',
+//         author:"J K Rowling",
+//         genre:'Fantasy',
+//         img:'harry.jpg'
+//     },
+//     {
+//         title:'Paathummayude Aadu',
+//         author:"Basheer",
+//         genre:'Novel',
+//         img:'basheer.jpg'
+//     }
+// ];
 booksRouter.get('/',(req,res)=>{
-     res.render("books",{nav,title:'Library',books
-    }); //books array
+    Bookdata.find().then((books)=>{
+        res.render("books",{nav,title:'Books',books});
+    });
+      //books array
 });
 booksRouter.get('/:id',(req,res)=>{
     const id = req.params.id;
-    res.render("book",{nav,title:'Books',book: books[id]});
+    Bookdata.findOne({_id:id}).then((book)=>{
+    res.render("book",{nav,title:'Books',book,id});
 });
+})
     return booksRouter;
 }
 module.exports = router;
